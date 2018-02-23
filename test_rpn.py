78ed63daf34bd51f29ba3d693f8d705394bc730a
belja <belja@umich.edu>
2018-02-23 10:21:41 -0500

    added subtraction test (subtraction not implemented)

--- test_rpn.py
+++ test_rpn.py
@@ -6,5 +6,8 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
     def test_adds(self):
-        result = rprn.calculate('1 1 + 2 +')
+        result = rpn.calculate('1 1 + 2 +')
         self.assertEqual(4, result)
+    def test_subtract(self):
+        result = rpn.calculate('5 2 -')
+        self.assertEqual(3, result)
