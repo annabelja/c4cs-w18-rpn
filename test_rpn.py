739df11c580f924dd5c634fd9d294308424b6423
belja <belja@umich.edu>
2018-03-05 21:24:20 -0500

    version of rpn pre-adding carat operator

--- test_rpn.py
+++ test_rpn.py
@@ -30,9 +30,9 @@ class TestBasics(unittest.TestCase):
     def test_mixofoperators(self):
         result = rpn.calculate('10 5 / 2 +')
         self.assertEqual(4, result)
-    def test_power(self):
-        result = rpn.calculate('2 1 ^')
-        self.assertEqual(2, result)
+#    def test_power(self):
+ #       result = rpn.calculate('2 1 ^')
+  #      self.assertEqual(2, result)
 #    def test_intDivision(self):
  #       result = rpn.calculate('5 2 .')
   #      self.assertEqual(2, result)
