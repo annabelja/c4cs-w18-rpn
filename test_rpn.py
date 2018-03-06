e1224513f88a8f74e4349feddce4ff464708138a
belja <belja@umich.edu>
2018-03-05 21:28:12 -0500

    cleaned out commented out tests

--- test_rpn.py
+++ test_rpn.py
@@ -5,10 +5,6 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
-#    def test_sum(self):
- #       rpn.calculate('1 1 -')
-  #      result = rpn.calculate('1 s')
-   #     self.assertEqual(1, result)
     def test_adds(self):
         result = rpn.calculate('1 1 + 2 +')
         self.assertEqual(4, result)
@@ -33,18 +29,9 @@ class TestBasics(unittest.TestCase):
 #    def test_power(self):
  #       result = rpn.calculate('2 1 ^')
   #      self.assertEqual(2, result)
-#    def test_intDivision(self):
- #       result = rpn.calculate('5 2 .')
-  #      self.assertEqual(2, result)
     def test_bitwiseAnd(self):
         result = rpn.calculate('1 1 &')
         self.assertEqual(1, result)
     def test_bitwiseOr(self):
         result = rpn.calculate('1 0 |')
         self.assertEqual(1, result)
-#    def test_bitwiseNot(self):
- #       result = rpn.calculate('1 !')
-  #      self.assertEqual(0, result)
-   # def test_copy(self):
-    #    rpn.calculate('1 1 +')
-     #   self.assertEqual(2, 2)
